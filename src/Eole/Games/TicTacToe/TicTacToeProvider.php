bcf26677924992dbafaafcb796130097212fc4f0
Julien Maulny <doubjulien@hotmail.fr>
2016-02-13 21:02:37 +0100

    refactor tictactoe services in service provider

--- src/Eole/Games/TicTacToe/TicTacToeProvider.php
+++ src/Eole/Games/TicTacToe/TicTacToeProvider.php
@@ -15,23 +15,5 @@ class TicTacToeProvider implements ServiceProviderInterface
             __DIR__.'/serializer',
             'Alcalyn\\TicTacToe'
         );
-
-        $tictactoePartiesFactory = function ($topicPath, array $arguments) use ($app) {
-            $partyId = $arguments['party_id'];
-            $partyRepository = $app['orm.em']->getRepository('Eole:Party');
-            $party = $partyRepository->findFullPartyById($partyId, 'tictactoe');
-            $partyManager = $app['eole.party_manager'];
-
-            return new Topic($topicPath, $party, $partyManager, $partyRepository);
-        };
-
-        $app['eole.games.tictactoe.topic.factory'] = $app->protect($tictactoePartiesFactory);
-
-        $app['eole.websocket.routes']->add('eole_games_tictactoe_party', new TopicRoute(
-            'eole/games/tictactoe/parties/{party_id}',
-            'eole.games.tictactoe.topic.factory',
-            array(),
-            array('party_id' => '^[0-9]+$')
-        ));
     }
 }
---
+++ src/Eole/Games/TicTacToe/TicTacToeWebsocketProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Eole\Games\TicTacToe;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Eole\WebSocket\Routing\TopicRoute;
+use Eole\Games\TicTacToe\Topic;
+
+class TicTacToeWebsocketProvider implements ServiceProviderInterface
+{
+    public function register(Container $app)
+    {
+        $tictactoePartiesFactory = function ($topicPath, array $arguments) use ($app) {
+            $partyId = $arguments['party_id'];
+            $partyRepository = $app['orm.em']->getRepository('Eole:Party');
+            $party = $partyRepository->findFullPartyById($partyId, 'tictactoe');
+            $partyManager = $app['eole.party_manager'];
+
+            return new Topic($topicPath, $party, $partyManager, $partyRepository);
+        };
+
+        $app['eole.games.tictactoe.topic.factory'] = $app->protect($tictactoePartiesFactory);
+
+        $app['eole.websocket.routes']->add('eole_games_tictactoe_party', new TopicRoute(
+            'eole/games/tictactoe/parties/{party_id}',
+            'eole.games.tictactoe.topic.factory',
+            array(),
+            array('party_id' => '^[0-9]+$')
+        ));
+    }
+}
